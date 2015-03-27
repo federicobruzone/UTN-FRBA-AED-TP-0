d0bf2b05f1876346d39fd1678ef743a74e4eea13
federicobruzone <federicobruzone@yahoo.com.ar>
2015-03-27 15:17:55 -0300

    Create HolaMundo.cpp

---
+++ HolaMundo.cpp
@@ -0,0 +1,16 @@
+/* UTN-FRBA
+Algoritmos y Estructura de Datos
+Curso K1091
+Alumno Federico Bruzone. Legajo [155.683-6]
+TP Nº 0
+*/
+
+#include <iostream>
+
+int main(){
+
+std::cout<<"Hola Mundo!\n";
+
+return 0;
+
+}
